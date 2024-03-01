4464954d248db7a0ac489fc5dffb43989674557f
mister2fresh <152710569+mister2fresh@users.noreply.github.com>
2024-03-01 10:30:35 -0700

    Add operators to variables in words to assign value

--- myscripts.js
+++ myscripts.js
@@ -4,14 +4,31 @@
 
 let words = ['rock',
 'paper',
-'scissors',
+'scissors',]
 
-];
+let 'rock' > 'scissors',
+ 'paper' > 'rock',
+ 'scissor' > 'paper';
+ 
 function getComputerChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
     console.log(getComputerChoice(words));
 
-/ randomly selects rock, paper, scissors /
+
+
+const computerSelection = getComputerChoice();
+let playerSelection = prompt("What's your play?")
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+    return 
+    
+  }
+  
+  const playerSelection = "rock";
+  console.log(playRound(playerSelection, computerSelection));
 
