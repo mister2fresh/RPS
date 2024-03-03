cc4e06d41eb548501389472b393d7fba461daa2b
mister2fresh <152710569+mister2fresh@users.noreply.github.com>
2024-03-03 09:27:06 -0700

    Create playRound function, enter parameters and test

--- myscripts.js
+++ myscripts.js
@@ -6,29 +6,51 @@ let words = ['rock',
 'paper',
 'scissors',]
 
-let 'rock' > 'scissors',
- 'paper' > 'rock',
- 'scissor' > 'paper';
- 
 function getComputerChoice(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
-
     console.log(getComputerChoice(words));
 
 
-
-const computerSelection = getComputerChoice();
-let playerSelection = prompt("What's your play?")
-
-
-
+let computerSelection = getComputerChoice(words);
+let playerSelection = 'rOcK'.toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
-    return 
     
-  }
-  
-  const playerSelection = "rock";
-  console.log(playRound(playerSelection, computerSelection));
+     if (playerSelection === computerSelection) {
+        alert('Tie!');
+     }
 
+     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        alert('You lose! Paper covers rock!');
+     }
+    
+
+     else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert('You win! Rock crushes scissors!');
+     }
+        
+     else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert('You win! Paper covers rock!');
+     }
+      
+    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert('You lose! Scissors cut paper!');
+    }
+    
+    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert('You lose! Rock crushes scissors!');
+    }
+
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert('You win! Scissors cut paper!');
+    }
+
+    else {
+        alert('I wanted to play a game');
+    }
+    }
+
+
+console.log(playRound(playerSelection, computerSelection));
+        
